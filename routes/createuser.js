2591829a29f85679bd39c3f2b51a0e3d485d84e0
Boatner <boatraj@dunwoody.edu>
2020-03-06 15:05:10 -0600

    sigh

---
+++ routes/createuser.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    //res.send('respond with a resource');
+
+    //the statement below contains a render function - the first argument is the view name and the second argument is an object with one key/value pair.
+    res.render('createuser', { title: 'Create Account'})
+});
+
+module.exports = router;
